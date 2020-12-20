c86fc21cfe6ae7eaa966e1d101f2b3cb4afe96e2
wang zhen yu <asdfg10510588@gmail.com>
2020-12-21 01:57:04 +0800

    新增 a1,a2路由及視圖

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/a1', function () {
+    return view('aa.a1');
+});
+
+Route::get('/a2', function () {
+    return view('aa.a2');
+});
